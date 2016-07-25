63521b1bae416e2ca25136e2770a8fada6859bd6
Charlie Baxter and Shradha Pulla <charlie+shradha@>
2016-07-25 11:00:36 -0700

    Add single account.cshtml, allow accounts to be updated

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,14 +6,41 @@ namespace JobBoard
   {
     public HomeModule()
     {
-      Get ["/"] = _ => View ["index.cshtml"];
+      Get ["/"] = _ => View ["index.cshtml", Account.GetAll()];
       Get ["/accounts/new"] = _ =>  View ["account_form.cshtml"];
       Get ["/jobs/new"] = _ => View ["job_form.cshtml"];
       Post ["/accounts"] = _ => {
+        Account newAccount = new Account
+        (
+          Request.Form ["account-first-name"],
+          Request.Form ["account-last-name"],
+          Request.Form ["account-email"],
+          Request.Form ["account-phone"],
+          Request.Form ["account-education"],
+          Request.Form ["account-resume"]
+        );
+        newAccount.Save();
         return View ["accounts.cshtml", Account.GetAll()];
       };
-      Post ["/jobs"] = _ => {
-        return View ["jobs.cshtml", Job.GetAll()];
+      Get ["/accounts/{id}/{first_name}"] = parameters => {
+        Account selectedAccount = Account.Find(parameters.id);
+        return View ["account.cshtml", selectedAccount];
+      };
+      Patch ["/accounts/{id}/{first_name}/updated"] = parameters => {
+        Account selectedAccount = Account.Find(parameters.id);
+        selectedAccount.Update
+        (
+        Request.Form ["account-first-name"],
+        Request.Form ["account-last-name"],
+        Request.Form ["account-email"],
+        Request.Form ["account-phone"],
+        Request.Form ["account-education"],
+        Request.Form ["account-resume"]
+        );
+        return View ["account.cshtml", selectedAccount];
+      };
+      Get ["/jobs"] = _ => {
+        return View ["jobs.cshtml"];
       };
     }
   }
