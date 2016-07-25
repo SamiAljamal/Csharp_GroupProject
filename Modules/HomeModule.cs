c230165b0f2b4bd10052d0150e4ac9d4bbab25b9
Charlie Baxter and Shradha Pulla <charlie+shradha@>
2016-07-25 10:39:35 -0700

    Connect first few pages in HomeModules.cs, create new accounts.cshtml & jobs.cshtml pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,7 +6,15 @@ namespace JobBoard
   {
     public HomeModule()
     {
-
+      Get ["/"] = _ => View ["index.cshtml"];
+      Get ["/accounts/new"] = _ =>  View ["account_form.cshtml"];
+      Get ["/jobs/new"] = _ => View ["job_form.cshtml"];
+      Post ["/accounts"] = _ => {
+        return View ["accounts.cshtml", Account.GetAll()];
+      };
+      Post ["/jobs"] = _ => {
+        return View ["jobs.cshtml", Job.GetAll()];
+      };
     }
   }
 }
