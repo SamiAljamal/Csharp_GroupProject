eb78b53cb5658e6191e02e3b0dfc4c02b2ad2326
Charlie Baxter and Shradha Pulla <charlie+shradha@>
2016-07-25 11:49:33 -0700

    Add username & password input forms to views pages & HomeModule.cs

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,44 +6,48 @@ namespace JobBoard
   {
     public HomeModule()
     {
-      // Get ["/"] = _ => View ["index.cshtml", Account.GetAll()];
-      // Get ["/accounts/new"] = _ =>  View ["account_form.cshtml"];
-      // Get ["/jobs/new"] = _ => View ["job_form.cshtml"];
-      // Post ["/accounts"] = _ => {
-      //   Account newAccount = new Account
-      //   (
-      //     Request.Form ["account-first-name"],
-      //     Request.Form ["account-last-name"],
-      //     Request.Form ["account-email"],
-      //     Request.Form ["account-phone"],
-      //     Request.Form ["account-education"],
-      //     Request.Form ["account-resume"]
-      //   );
-      //   newAccount.Save();
-      //   return View ["accounts.cshtml", Account.GetAll()];
-      // };
-      // Get ["/accounts/{id}/{first_name}"] = parameters => {
-      //   Account selectedAccount = Account.Find(parameters.id);
-      //   return View ["account.cshtml", selectedAccount];
-      // };
-      // Patch ["/accounts/{id}/{first_name}/updated"] = parameters => {
-      //   Account selectedAccount = Account.Find(parameters.id);
-      //   selectedAccount.Update
-      //   (
-      //   Request.Form ["account-first-name"],
-      //   Request.Form ["account-last-name"],
-      //   Request.Form ["account-email"],
-      //   Request.Form ["account-phone"],
-      //   Request.Form ["account-education"],
-      //   Request.Form ["account-resume"]
-      //   );
-      //   return View ["account.cshtml", selectedAccount];
-      // };
-      // Post ["/accounts/{id}/{first_name}/deleted"] = parameters => {
-      //   Account selectedAccount = Account.Find(parameters.id);
-      //   selectedAccount.DeleteOne();
-      //   return View ["deleted_account.cshtml", selectedAccount];
-      // };
+      Get ["/"] = _ => View ["index.cshtml", Account.GetAll()];
+      Get ["/accounts/new"] = _ =>  View ["account_form.cshtml"];
+      Get ["/jobs/new"] = _ => View ["job_form.cshtml"];
+      Post ["/accounts"] = _ => {
+        Account newAccount = new Account
+        (
+          Request.Form ["account-first-name"],
+          Request.Form ["account-last-name"],
+          Request.Form ["account-email"],
+          Request.Form ["account-phone"],
+          Request.Form ["account-education"],
+          Request.Form ["account-resume"],
+          Request.Form ["account-username"],
+          Request.Form ["account-password"]
+        );
+        newAccount.Save();
+        return View ["accounts.cshtml", Account.GetAll()];
+      };
+      Get ["/accounts/{id}/{first_name}"] = parameters => {
+        Account selectedAccount = Account.Find(parameters.id);
+        return View ["account.cshtml", selectedAccount];
+      };
+      Patch ["/accounts/{id}/{first_name}/updated"] = parameters => {
+        Account selectedAccount = Account.Find(parameters.id);
+        selectedAccount.Update
+        (
+        Request.Form ["account-first-name"],
+        Request.Form ["account-last-name"],
+        Request.Form ["account-email"],
+        Request.Form ["account-phone"],
+        Request.Form ["account-education"],
+        Request.Form ["account-resume"],
+        Request.Form ["account-username"],
+        Request.Form ["account-password"]
+        );
+        return View ["account.cshtml", selectedAccount];
+      };
+      Post ["/accounts/{id}/{first_name}/deleted"] = parameters => {
+        Account selectedAccount = Account.Find(parameters.id);
+        selectedAccount.DeleteOne();
+        return View ["deleted_account.cshtml", selectedAccount];
+      };
       // Post ["/jobs"] = _ => {
       //   Job newJob = new Job
       //   (
