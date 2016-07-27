754d7aad82502b8c18f13f85304e810d24b8092e
Charlie Baxter and Charlie Ewel <charlie+charlie@>
2016-07-27 13:11:51 -0700

    add working route for viewing ranked job matches

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using System.Collections.Generic;
 
 namespace JobBoard
 {
@@ -23,8 +24,7 @@ namespace JobBoard
           Request.Form ["account-phone"],
           Request.Form ["account-education"],
           Request.Form ["account-resume"],
-          Request.Form ["account-username"],
-          Request.Form ["account-password"]
+          Request.Form ["account-username"]
         );
         newAccount.Save();
         return View ["accounts.cshtml", Account.GetAll()];
@@ -62,34 +62,40 @@ namespace JobBoard
       Get ["/jobs"] = _ => {
         return View ["jobs.cshtml", Job.GetAll()];
       };
-      // Post ["/jobs"] = _ => {
-      //   Job newJob = new Job
-      //   (
-      //     Request.Form ["job-title"],
-      //     Request.Form ["job-description"],
-      //     Request.Form ["job-salary"]
-      //   );
-      //   newJob.Save();
-      //   return View ["jobs.cshtml", Job.GetAll()];
-      // };
+      Post ["/jobs"] = _ => {
+        Job newJob = new Job
+        (
+          Request.Form ["job-title"],
+          Request.Form ["job-description"],
+          Request.Form ["job-salary"]
+        );
+        newJob.Save();
+        return View ["jobs.cshtml", Job.GetAll()];
+      };
       Get ["/jobs/{id}/{title}"] = parameters => {
         Job selectedJob = Job.Find(parameters.id);
         return View ["job.cshtml", selectedJob];
       };
-      // Patch ["/jobs/{id}/{title}/updated"] = parameters => {
-      //   Job selectedJob = Job.Find(parameters.id);
-      //   selectedJob.Update
-      //   (
-      //     Request.Form ["job-title"],
-      //     Request.Form ["job-description"],
-      //     Request.Form ["job-salary"]
-      //   );
-      //   return View ["job.cshtml", selectedJob];
-      // };
+      Patch ["/jobs/{id}/{title}/updated"] = parameters => {
+        Job selectedJob = Job.Find(parameters.id);
+        selectedJob.Update
+        (
+          Request.Form ["job-title"],
+          Request.Form ["job-description"],
+          Request.Form ["job-salary"]
+        );
+        return View ["job.cshtml", selectedJob];
+      };
       Post ["/jobs/{id}/{title}/deleted"] = parameters => {
         Job selectedJob = Job.Find(parameters.id);
         selectedJob.Delete();
         return View ["deleted_job.cshtml", selectedJob];
+      };
+
+      Get ["/accounts/{id}/rankedjobs"] = parameters => {
+        Account selectedAccount = Account.Find(parameters.id);
+        Dictionary<Job, int> rankedJobs = selectedAccount.GetRankedJobs();
+        return View ["ranked.cshtml", rankedJobs];
       };
     }
   }
